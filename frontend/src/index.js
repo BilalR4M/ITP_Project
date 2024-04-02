45317d0de17b6fadc91ae794851f4e1fa3c6a4e6
BilalR4M <it22548900@my.sliit.lk>
2024-04-02 08:55:35 +0530

    dafa

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { TasksContextProvider } from './Context/TasksContext';
-
+import { AuthContextProvider } from './Context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TasksContextProvider>
-      <App />
-    </TasksContextProvider>
+    <AuthContextProvider>
+      <TasksContextProvider>
+        <App />
+      </TasksContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
+
 );
 
